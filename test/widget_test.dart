910c270ab96ca4a8ecfec59fec06dd42f680f364
Andres Felipe Rojas <107456253+Bakudi@users.noreply.github.com>
2025-12-10 16:11:20 -0500

    pruebas unitarias test 1

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,31 +1,57 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:eco_monitor/main.dart';
-import 'package:shimmer/main.dart';
+import 'package:eco_monitor/features/weather/presentation/pages/alerts_page.dart';
+import 'package:eco_monitor/features/weather/presentation/pages/favorites_page.dart';
+import 'package:eco_monitor/features/weather/presentation/pages/home_page.dart';
+import 'package:eco_monitor/features/weather/presentation/pages/settings_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('AlertsPage renders correctly', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const ProviderScope(
+        child: MaterialApp(
+          home: AlertsPage(),
+        ),
+      ),
+    );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.byType(AlertsPage), findsOneWidget);
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  testWidgets('FavoritesPage renders correctly', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const ProviderScope(
+        child: MaterialApp(
+          home: FavoritesPage(),
+        ),
+      ),
+    );
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.byType(FavoritesPage), findsOneWidget);
+  });
+
+  testWidgets('HomePage renders correctly', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const ProviderScope(
+        child: MaterialApp(
+          home: HomePage(),
+        ),
+      ),
+    );
+
+    expect(find.byType(HomePage), findsOneWidget);
+  });
+
+  testWidgets('SettingsPage renders correctly', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const ProviderScope(
+        child: MaterialApp(
+          home: SettingsPage(),
+        ),
+      ),
+    );
+
+    expect(find.byType(SettingsPage), findsOneWidget);
   });
 }
